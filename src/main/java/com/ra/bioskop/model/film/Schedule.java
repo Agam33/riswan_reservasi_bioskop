14243773b5a14ac2c342eb5533828fc1f3005735
Agam33 <riswanagam@gmail.com>
2022-09-23 02:09:08 +0700

    update readme.md

--- src/main/java/com/ra/bioskop/model/film/Schedule.java
+++ src/main/java/com/ra/bioskop/model/film/Schedule.java
@@ -1,6 +1,5 @@
 package com.ra.bioskop.model.film;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +8,6 @@ import lombok.Setter;
 import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.Date;
 
