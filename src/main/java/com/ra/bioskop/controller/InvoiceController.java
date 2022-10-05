37fc4bda6650c57e1b1e23fa0819f5b2413db7e0
Agam33 <riswanagam@gmail.com>
2022-10-06 00:57:55 +0700

    #doc-InvoiceController

--- src/main/java/com/ra/bioskop/controller/InvoiceController.java
+++ src/main/java/com/ra/bioskop/controller/InvoiceController.java
@@ -3,6 +3,12 @@ package com.ra.bioskop.controller;
 import com.ra.bioskop.dto.model.fileDB.FileDB;
 import com.ra.bioskop.dto.response.ResponseError;
 import com.ra.bioskop.service.InvoiceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import net.sf.jasperreports.engine.JRException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ByteArrayResource;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 
+@Tag(name = "Invoice")
 @RestController
 @RequestMapping("/api/invoice")
 public class InvoiceController {
@@ -21,6 +28,12 @@ public class InvoiceController {
     @Autowired
     private InvoiceService invoiceService;
 
+    @Operation(summary = "Download invoice file")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "berhasil download"),
+            @ApiResponse(responseCode = "204", description = "tidak ada content",
+                    content = {@Content(mediaType = "application/json",
+                            schema = @Schema(implementation = ResponseError.class))}) })
     @GetMapping("/download")
     public ResponseEntity<?> downloadInvoiceFile(@RequestParam("fileName") String fileName) {
         try {
