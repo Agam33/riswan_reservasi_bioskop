e08027bd71bcf8ce42da13abbbf5899b0e88a5b1
Agam33 <riswanagam@gmail.com>
2022-11-03 14:00:50 +0700

    #update firebase-config

--- src/main/java/com/ra/bioskop/firebase/FirebaseConfig.java
+++ src/main/java/com/ra/bioskop/firebase/FirebaseConfig.java
@@ -1,8 +1,32 @@
 package com.ra.bioskop.firebase;
 
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("{app.firebase.file}")
+    private String fbFile;
+
+    @Primary
+    @Bean
+    public FirebaseApp firebaseApp() throws IOException {
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(
+                        new ClassPathResource(fbFile).getInputStream()))
+                .build();
+        if(FirebaseApp.getApps().isEmpty())
+            FirebaseApp.initializeApp(options);
+        return FirebaseApp.getInstance();
+    }
+    
 }
